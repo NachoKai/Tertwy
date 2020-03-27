edefd5004bdce65f67111d841ee508a1b0c1ccb8
Nacho Caiafa <nachokai@outlook.com>
2020-03-27 19:06:19 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Tertwy/precache-manifest.0e1c76dabd145ed11443d472c0727aef.js"
+  "/Tertwy/precache-manifest.2faa6daf481aff1f9a37402444555d4d.js"
 );
 
 self.addEventListener('message', (event) => {
